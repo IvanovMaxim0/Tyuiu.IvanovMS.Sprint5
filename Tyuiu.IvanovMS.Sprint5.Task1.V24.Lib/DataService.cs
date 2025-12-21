b806342938b5fdab5b3806546a71197f084be1d6
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-21 14:22:09 +0500

    Task1

---
+++ Tyuiu.IvanovMS.Sprint5.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovMS.Sprint5.Task1.V24.Lib
+{
+    public class DataService : ISprint5Task1V24
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.GetTempFileName();
+            
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Exists(path);
+            }
+
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(((3 * Math.Cos(x)) / (4 * x - 0.5)) + Math.Sin(x) - 5 * x - 2, 2);
+                strY = Convert.ToString(y);
+                if (x != stopValue && (4 * x - 0.5) != 0)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else if (x != stopValue && (4 * x - 0.5) == 0)
+                {
+                    y = 0;
+                    strY = Convert.ToString(y);
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint5.Task1.V24/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.IvanovMS.Sprint5.Task1.V24.Lib;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int startValue = -5;
+        int stopValue = 5;
+        Console.Title = "Спринт #5 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Класс File. Запись данных в текстовый файл                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дана функция, (произвести табулирование) f(x) на заданном диапазоне     *");
+        Console.WriteLine("* [-5;5] с шагом 1. Произвести проверку деления на 0. При делении на 0    *");
+        Console.WriteLine("* вернуть значение 0.                                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* F(x) = 3cos(x) / 4x - 0.5 + sin(x) - 5x - 2                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(startValue, stopValue);
+        Console.WriteLine("Файл:" + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
