f066570624d6113790dfd7246ecba9612f6ce84b
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-23 17:11:08 +0500

    Task3

--- Tyuiu.IvanovMS.Sprint5.Task3.V7.Lib/DataService.cs
+++ Tyuiu.IvanovMS.Sprint5.Task3.V7.Lib/DataService.cs
@@ -6,7 +6,16 @@ namespace Tyuiu.IvanovMS.Sprint5.Task3.V7.Lib
     {
         public string SaveToFileTextData(int x)
         {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            
+            double y = 1.6 * (Math.Pow(x, 3)) - 2.1 * (Math.Pow(x, 2)) + 7 * x;
 
+            y = Math.Round(y, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
         }
     }
 }
--- Tyuiu.IvanovMS.Sprint5.Task3.V7/Program.cs
+++ Tyuiu.IvanovMS.Sprint5.Task3.V7/Program.cs
@@ -1,1 +1,30 @@
 ﻿using Tyuiu.IvanovMS.Sprint5.Task3.V7.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Двумерные массивы  (статический ввод)                                   *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найдите значение 1,6 * x^3 - 2,1 * x^2 + 7 * x.                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* x = 2                                                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int x = 2;
+        Console.WriteLine(ds.SaveToFileTextData(x));
+
+    }
+}
