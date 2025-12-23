b4ec424b1ca8c855dccf195160cc9952ac103bbc
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-23 20:05:50 +0500

    Task7

---
+++ Tyuiu.IvanovMS.Sprint5.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.IvanovMS.Sprint5.Task7.V22.Lib;
+namespace Tyuiu.IvanovMS.Sprint5.Task7.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint5.Task7.V22/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.IvanovMS.Sprint5.Task7.V22.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Двумерные массивы  (статический ввод)                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask7V22.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.LoadDataAndSave(path));
+    }
+}
